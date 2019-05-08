063e2b90118e9897f32e06faa375880e03237bbb
aldiar bolat <bolat.aldiar1@gmail.com>
2019-05-08 08:55:16 +0600

    Jk

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from api import views
+
+urlpatterns = [
+    path('status', views.status),
+    path('position', views.position),
+    re_path(r'user/(\d)', views.UserView.as_view()),
+    re_path(r'company/(\d)', views.CompanyView.as_view()),
+    path('company', views.CompanyView.as_view()),
+
+]
---
+++ api/views.py
@@ -0,0 +1,80 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+from api.models import User, Company
+from api.serializers import StatusSerializer, PositionSerializer, UserSerializer, CompanySerializer
+
+
+@api_view(['POST'])
+def status(request):
+    if request.method == 'POST':
+        serializer = StatusSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+@api_view(['POST'])
+def position(request):
+    if request.method == 'POST':
+        serializer = PositionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+class UserView(APIView):
+    def get_object(self, pk):
+        try:
+           return User.objects.get(id=pk)
+        except  User.DoesNotExist:
+           raise Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class CompanyView(APIView):
+    def get_object(self, pk):
+        try:
+           return Company.objects.get(id=pk)
+        except  Company.DoesNotExist:
+           raise Http404
+
+    def get(self, request, pk):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def put(self, request, pk):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(instance=company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        company = self.get_object(pk)
+        company.delete()
+        return Response({})
+
